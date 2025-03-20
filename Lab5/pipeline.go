e9025e2d8c95fcd29734edb9e42c3b3c217ded97
yallecarvalho <yalle.carvalho@ccc.ufcg.edu.br>
2025-03-19 21:03:41 -0300

    adicionando comentarios e funcoes de produtor e consumidor

--- Lab5/pipeline.go
+++ Lab5/pipeline.go
@@ -34,12 +34,34 @@ func isLetter(s string) bool {
 	return true
 }
 
-func produce(n int){
-    for {
-            fmt.Println(RandString(n))
+//gera 100 strings aleatorias de 5 caracteres usando o RandString e envia cada uma para o canal out, depois, fecha o canal sinalizando que nao haverá mais dados
+func produce(out chan string){ //envia strings pra um canal
+    for i := 0; i < 100; i++ {
+            out <- RandString(5) //guarda os valores gerados por RandString em out
     }
+	close(out) //fecha o canal pra ter um livelock
 }
 
+func filtrarAlphas(in chan string, out chan string) { //filtra de um canal de input e envia por um canal de output
+	for word := range in { // le strings do canal de entrada (in)
+		if isLetter(word){ //verifica se a string contem apenas letras
+			out <- word //envia pro canal de saida
+		}
+	}
+	close(out)  // IMPORTANTE fechar o canal depois que todos os dados forem processados
+}
+
+
 func main() {
-    produce(5)
+    rawContent := make(chan string)  //cria canal pra strings nao filtradas
+	filtrarAlphas := make(chan string)  //cria canal pra strings filtradas (somente letras)
+
+	go produce(rawContent)  //inicia a go routine para produzir strings
+	go filtrarAlphas(rawContent, filteredAlphas)   //inicia a goroutine para filtrar strings
+
+	// Consumir e imprimir os valores filtrados
+	for alpha := range filetredAlphas {   //le do canal filtrado e imprime
+		fmt.Printf("alpha:  <%s>\n", <- alpha)
+	}
+
 }
